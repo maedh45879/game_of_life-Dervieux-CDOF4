cd98dc1eb7869a52a2fde4809a31ec747de2fd88
maedh45879 <manon.aubry@edu.devinci.fr>
2025-01-13 19:38:55 +0100

    improve the project

--- init.py
+++ init.py
@@ -1,9 +1,9 @@
 from game_of_life import Game_of_life
 game = Game_of_life()
 
-size = int(input('Quelle taille de grille voulez-vous ?\n'))
-pop = float(input('Quelle part de population initiale (<1)?\n'))
+size = int(input('Veuillez entrer la taille de la grille (par exemple, 100 pour une grille 100x100) :\n'))
+pop = float(input('Quelle proportion de cellules vivantes initiales souhaitez-vous ? (entre 0 et 1, par exemple 0.2 pour 20%) :\n'))
 game.__init__(size)
-iterations = int(input('Combien d\'itérations voulez-vous ?\n'))
+iterations = int(input('Combien de cycles d\'itérations voulez-vous que le jeu exécute ? (par exemple, 50) :\n'))
 game.run(iterations)
 
